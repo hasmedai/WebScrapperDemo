573bcccc1596a073cd4714fd0376ec8638ad7516
HARRY MASIDZA <harry.masidza@centrino.co.ke>
2023-04-06 20:28:22 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # WebScrapperDemo
-multi-threaded web scraping application using HttpClient, HtmlAgilityPack, and ConcurrentBag in C#. This code scrapes data from a website and stores it in a concurrent collection for parallel processing.
+Multi-threaded web scraping application using HttpClient, HtmlAgilityPack, and ConcurrentBag in C#. 
+This code scrapes data from a website and stores it in a concurrent collection for parallel processing.
---
+++ WebScrapper/Program.cs
@@ -0,0 +1,84 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Concurrent;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static readonly ConcurrentBag<string> scrapedData = new ConcurrentBag<string>();
+
+        static async Task Main(string[] args)
+        {
+            string url = "https://example.com";
+
+            int maxConcurrentRequests = 5;
+
+            using (var httpClient = new HttpClient())
+            {
+                var html = await httpClient.GetStringAsync(url);
+
+                var htmlDocument = new HtmlDocument();
+
+                htmlDocument.LoadHtml(html);
+
+                var links = htmlDocument.DocumentNode.Descendants("a");
+
+                SemaphoreSlim semaphore = new SemaphoreSlim(maxConcurrentRequests);
+
+                var tasks = new Task[links.Count()];
+
+                int i = 0;
+
+                foreach (var link in links)
+                {
+                    await semaphore.WaitAsync();
+
+                    tasks[i++] = ScrapeDataAsync(httpClient, link, semaphore);
+                }
+
+                await Task.WhenAll(tasks);
+            }
+
+            Console.WriteLine("Scraping completed. Scraped data:");
+
+            foreach (var data in scrapedData)
+            {
+                Console.WriteLine(data);
+            }
+        }
+
+        static async Task ScrapeDataAsync(HttpClient httpClient, HtmlNode link, SemaphoreSlim semaphore)
+        {
+            try
+            {
+                var href = link.GetAttributeValue("href", string.Empty);
+
+                if (!string.IsNullOrEmpty(href))
+                {
+                    var linkHtml = await httpClient.GetStringAsync(href);
+
+                    var linkHtmlDocument = new HtmlDocument();
+
+                    linkHtmlDocument.LoadHtml(linkHtml);
+
+                    // Extract data from linkHtmlDocument and store it in scrapedData
+                    // For example:
+                    var data = linkHtmlDocument.DocumentNode.Descendants("div")
+                        .Select(div => div.InnerText)
+                        .FirstOrDefault();
+
+                    if (!string.IsNullOrWhiteSpace(data))
+                        scrapedData.Add(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to scrape data from link: {ex.Message}");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
